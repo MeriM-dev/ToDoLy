8daaf7006ff732f2869e7414bab9d7d0c3364f34
MeriM-dev <meri.vancovska@gmail.com>
2019-10-10 22:06:44 +0200

    Add initial class diagram

--- src/com/meri/todoly/Task.java
+++ src/com/meri/todoly/Task.java
@@ -6,7 +6,7 @@ public class Task {
     private String projectName;
     private String status;
 
-    public Task(String name,String date,String projectName){
+    public Task(String name, String date, String projectName){
         this.name = name;
         this.date = date;
         this.projectName = projectName;
